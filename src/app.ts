3f971cd8feeea2888823fa9cc3a5008bdc9f897e
Chris Houdeshell (choudeshell) <choudeshell@gmail.com>
2013-04-16 22:59:44 -0400

    Add CSS & Help

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,19 @@
 declare var chrome: any;
 declare var js_beautify: any;
 
+var output = ace.edit("js-output");
+output.setTheme("ace/theme/chrome");
+output.getSession().setUseWorker(false);
+output.getSession().setMode("ace/mode/javascript");
+output.setReadOnly(true);
+
 var editor = ace.edit("ts-editor");
 editor.setTheme("ace/theme/chrome");
 editor.getSession().setUseWorker(false);
 editor.getSession().setMode("ace/mode/typescript");
 editor.getSession().setUseSoftTabs(true);
 
-var output = ace.edit("js-output");
-output.setTheme("ace/theme/chrome");
-output.getSession().setUseWorker(false);
-output.getSession().setMode("ace/mode/javascript");
-output.setReadOnly(true);
+
 
 var executeConsole = () => {
     var js = compliatiionService.compile(editor.getValue());
